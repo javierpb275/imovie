5c3c5c1a62695c8fa822a69caf23220f4e78f4a4
javierpb275 <javierpb275@gmail.com>
2022-03-23 19:14:29 +0100

    created home and route / to home and changed styles

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import SignInView from '../views/SignInView.vue';
+import HomeView from '../views/HomeView.vue';
 
 const routes: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeView
+    },
     {
         path: '/',
         name: 'SignIn',
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1 class="text-red-700 text-5xl font-bold mb-40 mt-20">IMOvie</h1>
+        <button
+            class="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded mb-4"
+        >Sign In</button>
+        <router-link to="/signup">
+        <p class="mb-4 text-red-700">Don't have an account yet? Sign up!</p>
+        </router-link>
+    </div>
+</template>
