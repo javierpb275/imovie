3cd20ed852dab724734eda4661869ea43479f9cf
Javier Pardinas <javier.pardinas@colex.local>
2022-03-24 14:51:22 +0100

    created signup view and added route to signup view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import SignInView from '../views/SignInView.vue';
 import HomeView from '../views/HomeView.vue';
+import SignInView from '../views/SignInView.vue';
+import SignUpView from '../views/SignUpView.vue';
 
 const routes: RouteRecordRaw[] = [
     {
@@ -12,6 +13,11 @@ const routes: RouteRecordRaw[] = [
         path: '/signin',
         name: 'SignIn',
         component: SignInView
+    },
+    {
+        path: '/signup',
+        name: 'SignUp',
+        component: SignUpView
     }
 ];
 
