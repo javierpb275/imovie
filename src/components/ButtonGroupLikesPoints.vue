4fdd383d8cb144c9f1306f2a6fb27085135b401c
javierpb275 <javierpb275@gmail.com>
2022-03-28 18:36:43 +0200

    created buttongrouplikespoints components and added it in buttongroupusersfollowees components

--- src/components/ButtonGroupLikesPoints.vue
+++ src/components/ButtonGroupLikesPoints.vue
@@ -1,37 +1,14 @@
-<script setup lang="ts">
-import CustomSVG from './CustomSVG.vue';
-import ButtonGroupLikesPoints from './ButtonGroupLikesPoints.vue';
-import { ref } from 'vue'
-
-const upDownArrow = ref<string>('down-arrow');
-
-const showOrderButtonGroup = () => {
-    upDownArrow.value = upDownArrow.value === "down-arrow" ?
-        "up-arrow" : "down-arrow";
-}
-</script>
-
 <template>
-<div>
     <div class="flex items-center justify-center mb-3">
         <div class="inline-flex shadow-md hover:shadow-lg focus:shadow-lg" role="group">
             <button
                 type="button"
                 class="rounded-l inline-block px-6 py-2.5 bg-red-700 text-white font-medium text-xs leading-tight uppercase hover:bg-red-800 focus:bg-red-800 focus:outline-none focus:ring-0 active:bg-red-900 transition duration-150 ease-in-out"
-            >People you follow</button>
+            > Order by Likes</button>
             <button
                 type="button"
                 class="rounded-r inline-block px-6 py-2.5 bg-red-700 text-white font-medium text-xs leading-tight uppercase hover:bg-red-800 focus:bg-red-800 focus:outline-none focus:ring-0 active:bg-red-900 transition duration-150 ease-in-out"
-            >All users</button>
+            >Order by Points</button>
         </div>
-        <CustomSVG
-            :svgName="upDownArrow"
-            :class="'h-8 w-8 text-gray-900'"
-            @click="showOrderButtonGroup()"
-        />
-    </div>
-    <div v-if="upDownArrow === 'up-arrow'">
-        <ButtonGroupLikesPoints/>
-    </div>
     </div>
 </template>
