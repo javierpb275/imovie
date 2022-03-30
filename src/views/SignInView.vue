156dc9c1c0a6897efd66ba6d8988a00581797e8f
javierpb275 <javierpb275@gmail.com>
2022-03-30 16:59:14 +0200

    fixed signinview

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -35,28 +35,6 @@
             href="#"
           >Sign up</router-link>
         </div>
-        <div class="mb-6">
-          <input
-            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="password"
-            type="password"
-            placeholder="Password"
-          />
-          <!-- <p class="text-red-700 text-xs italic">Please choose a password.</p> -->
-        </div>
-      </div>
-      <div class="flex items-center justify-between">
-        <button
-          class="bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-10 rounded focus:outline-none focus:shadow-outline"
-          type="button"
-        >Sign In</button>
-        <div>
-          <router-link
-            to="/signup"
-            class="align-baseline font-bold text-sm text-red-700 hover:text-red-800"
-            href="#"
-          >Sign up</router-link>
-        </div>
       </div>
     </form>
   </div>
