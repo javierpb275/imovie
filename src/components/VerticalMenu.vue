9f0ece0af3c3096344255bdb83505d243e460bdf
Lucía L.A <lucialamalba@gmail.com>
2022-04-01 18:40:03 +0200

    vertical menu icons

--- src/components/VerticalMenu.vue
+++ src/components/VerticalMenu.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import CustomAvatar from './CustomAvatar.vue';
+import CustomSVG from './CustomSVG.vue';
+
+import { ref } from 'vue'
+
+const darkLightMode = ref<string>('switch-light-mode');
+
+const changeVisualMode = () => {
+    darkLightMode.value = darkLightMode.value === "switch-light-mode" ?
+        "switch-dark-mode" : "switch-light-mode";
+}
 </script>
 
 <template>
@@ -18,19 +28,41 @@ import CustomAvatar from './CustomAvatar.vue';
                 <a
                     href="#"
                     class="block py-4 mb-4 mx-2 rounded transition duration-200 hover:bg-gray-800"
-                >New Releases</a>
+                >
+                <CustomSVG :svgName="'movie-ticket'" :class="'text-white w-6 h-6 inline'"/>
+                New releases</a>
                 <a
                     href="#"
                     class="block py-4 mb-4 mx-2 rounded transition duration-200 hover:bg-gray-800"
-                >All Movies</a>
+                >
+                <CustomSVG :svgName="'movie'" :class="'text-white w-6 h-6 inline'"/>
+                All movies</a>
                 <a
                     href="#"
                     class="block py-4 mb-4 mx-2 rounded transition duration-200 hover:bg-gray-800"
-                >Favorite Opinions</a>
+                >
+                <CustomSVG :svgName="'users'" :class="'text-white w-6 h-6 inline'"/>
+                My followed users</a>
                 <a
                     href="#"
                     class="block py-4 mb-4 mx-2 rounded transition duration-200 hover:bg-gray-800"
-                >App Settings??</a>
+                >
+                <CustomSVG :svgName="'filled-heart'" :class="'text-white w-6 h-6 inline'"/>
+                Favorite opinions</a>
+                <a
+                    href="#"
+                    class="block py-4 mb-4 mx-2 rounded transition duration-200 hover:bg-gray-800"
+                >
+                <CustomSVG :svgName="'options'" :class="'text-white w-6 h-6 inline'"/>
+                Profile settings</a>
+
+
+            <!--Pendiente qarreglar menú vertical. Esto se sale fuera:-->
+                <CustomSVG
+                :svgName="darkLightMode"
+                :class="'h-8 w-8 text-black bg-slate-600'"
+                @click="changeVisualMode"
+                />
             </nav>
         </div>
     </div>
