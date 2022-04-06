9a52b4454ced9e884e7c49d7d8efb0ea1ad39342
Javier Pardinas <javier.pardinas@colex.local>
2022-04-06 11:31:25 +0200

    added reviewcardlist component to the movieview page

--- src/views/MovieView.vue
+++ src/views/MovieView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Movie from '../components/Movie.vue';
+import ReviewCardList from '../components/ReviewCardList.vue';
 </script>
 
 <template>
     <div>
         <Movie />
+        <h1 class="text-3xl">Opinions:</h1>
+        <ReviewCardList/>
     </div>
 </template>
