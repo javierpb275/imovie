4dbbe875e0f49468d437f1549e29618629cc1202
Lucía L.A <lucialamalba@gmail.com>
2022-05-13 12:45:09 +0200

    allavatars changes

--- src/components/AllAvatars.vue
+++ src/components/AllAvatars.vue
@@ -3,18 +3,10 @@
 </script>
 
 <template>
-<<<<<<< HEAD
-  <div class="lg:ml-28 mt-12 mx-auto">
-    <div class="grid grid-cols-4 items-center">
-      <div class="mb-4">
-        <img src="/img/avatars/default-avatar.PNG"
-          class="w-40 h-auto rounded-full hover:scale-110 transition duration-500 cursor-pointer" alt="">
-=======
 <div class="lg:ml-28 mx-auto">
     <div class="grid grid-cols-4 items-center">
       <div class="mb-4">
         <img src="/img/avatars/default-avatar.PNG" class="w-40 h-auto rounded-full hover:scale-110 transition duration-500 cursor-pointer" >
->>>>>>> bd0710a7b3f1ec80185fa292fbe33479925f4a08
       </div>
       <div class="mb-4">
         <img src="/img/avatars/avatar1.PNG"
--- src/views/MyProfileSettingsView.vue
+++ src/views/MyProfileSettingsView.vue
@@ -3,10 +3,7 @@ import { reactive, ref, onMounted } from "vue";
 import { IUserSignUp } from "../services/serviceTypes";
 import { useAuthStore } from "../stores/auth";
 import { AuthService } from "../services/authService";
-import AllAvatars from "../components/AllAvatars.vue";
-import { useRouter } from "vue-router";
-
-const router = useRouter()
+import AllAvatars from "../components/AllAvatars.vue"
 
 const errorMessage = ref<string>("");
 const authStore = useAuthStore();
@@ -14,7 +11,7 @@ const authStore = useAuthStore();
 
 onMounted(async () => {
   if (authStore.isAuthorized) {
-    const headers = await AuthService.getHeaderToken();
+    const headers = AuthService.getHeaderToken();
     try {
       const data = await authStore.getProfile(headers);
       if (data.error) {
@@ -22,42 +19,52 @@ onMounted(async () => {
         return;
       }
     } catch (err) {
-      AuthService.removeTokensAndClearStore();
-      router.push("/signin");
+      errorMessage.value = "Error Getting Profile";
     }
   }
 })
 
 const user = reactive<IUserSignUp>({
-  username: "",
-  email: "",
-  password: "",
-  avatar: "/img/avatars/default-avatar.PNG",
+    username: "",
+    email: "",
+    password: "",
+    avatar: "/img/avatars/default-avatar.PNG",
 });
 
 </script>
 
 <template>
-  <div class="mt-20 lg:mt-20 lg:ml-60 mx-auto">
-    <p class=" text-2xl font-bold mb-10">My profile settings</p>
-    <div class="mb-3">
-      <div class="inline-block mr-10 w-20">Username</div>
-      <input class="border-2 hover:border-gray-900 bg-white h-10 px-1 rounded-lg text-s focus:outline-none lg:w-2/4"
-        type="text" name="name" placeholder="theName" />
-    </div>
+    <div class="mt-20 lg:mt-20 lg:ml-60 mx-auto">
+        <p class=" text-2xl font-bold mb-10">My profile settings</p>
+        <div class="mb-3">
+            <div class="inline-block mr-10 w-20">Username</div>
+            <input class="border-2 hover:border-gray-900 bg-white h-10 px-1 rounded-lg text-s focus:outline-none lg:w-2/4"
+                type="text" name="name" placeholder="theName" />
+        </div>
 
-    <div class="mb-3">
-      <div class="inline-block mr-10 w-20">Email</div>
-      <input class="border-2 hover:border-gray-900 bg-white h-10 px-1 rounded-lg text-s focus:outline-none lg:w-2/4"
-        type="email" name="email" placeholder="theEmail" />
-    </div>
+        <div class="mb-3">
+            <div class="inline-block mr-10 w-20">Email</div>
+            <input class="border-2 hover:border-gray-900 bg-white h-10 px-1 rounded-lg text-s focus:outline-none lg:w-2/4"
+                type="email" name="email" placeholder="theEmail" />
+        </div>
 
-    <div class="mb-3">
-      <div class="inline-block mr-10 w-20">Password</div>
-      <input class="border-2 hover:border-gray-900 bg-white h-10 px-1 rounded-lg text-s focus:outline-none lg:w-2/4"
-        type="password" name="password" placeholder="thePassword" />
+        <div class="mb-3">
+            <div class="inline-block mr-10 w-20">Password</div>
+            <input class="border-2 hover:border-gray-900 bg-white h-10 px-1 rounded-lg text-s focus:outline-none lg:w-2/4"
+                type="password" name="password" placeholder="thePassword" />
+        </div>
+        <p class=" text-xl mb-3 mt-8 lg:my-10">Choose a new avatar:</p>
+
+        <AllAvatars/>
+
+        <button type="button"
+                class="rounded-lg inline-block my-10 lg:my-16 mx-5 px-5 py-2.5 hover:bg-slate-800 bg-slate-400 cursor-pointer text-white text-s leading-tight">
+                Cancel
+        </button>
+
+        <button type="button"
+                class="rounded-lg inline-block my-10 lg:my-16 mx-5 px-10 py-2.5 hover:bg-red-700 bg-red-800 cursor-pointer text-white text-s leading-tight">
+                Update it!
+        </button>
     </div>
-    <p class=" text-xl mt-12">Choose a new avatar:</p>
-    <AllAvatars />
-  </div>
 </template>
